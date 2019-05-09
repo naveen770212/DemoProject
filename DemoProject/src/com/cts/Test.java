a672a321f62262cf5a174106b63a6532f2a78632
CGI <CGI@CGI-JAVA-136>
2019-05-09 11:18:17 +0530

    Second Message

--- DemoProject/src/com/cts/Test.java
+++ DemoProject/src/com/cts/Test.java
@@ -3,7 +3,7 @@ package com.cts;
 public class Test {
 	public static void main(String[] args) {
 	
-		System.out.println("hiiiiiiii");
+		System.out.println("hiiiiiiii 1");
 		System.out.println("HELLO GITHUB");
 	}
 
